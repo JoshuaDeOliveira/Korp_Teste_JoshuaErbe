018a320825fe1de20514bc97fd773881f9e2a458
Joshua Erbe <joshuadultra@gmail.com>
2026-09-02 22:26:33 -0300

    Atts

--- frontend/notas-fiscais-app/src/main.ts
+++ frontend/notas-fiscais-app/src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js'; // precisa vir ANTES de qualquer outro import, senao da erro NG0908 na hora de subir
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ModuloRaiz } from './app/app.module';
 
